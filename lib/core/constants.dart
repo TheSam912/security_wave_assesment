92fce2391a2fe77ef00b8ec8f14329698e11fd6e
“sobhan”912 <sobhan912@gmail.com>
2025-05-15 14:11:10 +0330

    update default profile image url

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,7 +1,7 @@
 class AppStrings {
   static const String appName = "SecurityWave";
   static const String imageProfileUrl =
-      "https://bayanbox.ir/view/3866533947169516030/%D8%B9%DA%A9%D8%B3-%D9%BE%D8%B3%D8%B1-%D9%85%D8%B0%D9%87%D8%A8%DB%8C-%D8%AE%D9%88%D8%B4-%D8%AA%DB%8C%D9%BE.jpg";
+      "https://raw.githubusercontent.com/TheSam912/security_wave_assesment/main/assets/images/avatar.jpeg";
   static const String avatar =
       "https://raw.githubusercontent.com/TheSam912/security_wave_assesment/refs/heads/main/assets/images/avatar.jpeg";
 }
